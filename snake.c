69aac8b72aeb713d49a00a40512c996b39b8cbb0
Patryk Sztuczka <patryk.sztuczka@codeagency.pl>
2024-06-03 18:29:08 +0200

    Implemented base tail logic for static nodes

--- snake.c
+++ snake.c
@@ -1,122 +1,150 @@
+#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 #include <windows.h>
 
 const int BOARD_WIDTH = 40;
 const int BOARD_HEIGHT = 25;
 
-void generate_board(int snake_x, int snake_y, int fruit[])
-{
-    system("cls");
-    for (int i = 0; i < BOARD_HEIGHT; i++)
-    {
-        for (int j = 0; j < BOARD_WIDTH; j++)
-        {
-            if (i == 0 || i == BOARD_HEIGHT - 1)
-            {
-                printf("#");
-            }
-            else if ((i > 0 && i < BOARD_HEIGHT) && (j == 0 || j == BOARD_WIDTH - 1))
-            {
-                printf("#");
-            }
-            else if (snake_x == j && snake_y == i)
-            {
-                printf("0");
-            }
-            else if (fruit[0] == j && fruit[1] == i)
-            {
-                printf("*");
-            }
-            else
-            {
-                printf(" ");
-            }
-        }
-        printf("\n");
+typedef struct {
+  void *next;
+  int x;
+  int y;
+} SnakeNode;
+
+void generate_board(SnakeNode *snake_head, int fruit[]) {
+  system("cls");
+  SnakeNode *current = snake_head;
+  for (int i = 0; i < BOARD_HEIGHT; i++) {
+    for (int j = 0; j < BOARD_WIDTH; j++) {
+      if (i == 0 || i == BOARD_HEIGHT - 1) {
+        printf("#");
+      } else if ((i > 0 && i < BOARD_HEIGHT) &&
+                 (j == 0 || j == BOARD_WIDTH - 1)) {
+        printf("#");
+      } else if (current != NULL && current->x == j && current->y == i) {
+        printf("0");
+        current = current->next;
+      } else if (fruit[0] == j && fruit[1] == i) {
+        printf("*");
+      } else {
+        printf(" ");
+      }
     }
+    printf("\n");
+  }
 }
 
-void move(int *x, int *y, char *direction, int *is_game_over)
-{
-    Sleep(100);
-    if (kbhit())
-    {
-        char key = getch();
-        switch (key)
-        {
-        case 'w':
-            *y = *y - 1;
-            *direction = 'w';
-            break;
-        case 's':
-            *y = *y + 1;
-            *direction = 's';
-            break;
-        case 'a':
-            *x = *x - 1;
-            *direction = 'a';
-            break;
-        case 'd':
-            *x = *x + 1;
-            *direction = 'd';
-            break;
-        case 'x':
-            *is_game_over = 1;
-        default:
-            break;
-        }
+void move(SnakeNode *snake_head, char *direction, int *is_game_over) {
+  SnakeNode *current = snake_head;
+  int prev_x = current->x;
+  int prev_y = current->y;
+
+  Sleep(1000);
+  if (kbhit()) {
+    char key = getch();
+    switch (key) {
+      case 'w':
+        if (*direction == 's') break;
+        *direction = 'w';
+        break;
+      case 's':
+        if (*direction == 'w') break;
+        *direction = 's';
+        break;
+      case 'a':
+        if (*direction == 'd') break;
+        *direction = 'a';
+        break;
+      case 'd':
+        if (*direction == 'a') break;
+        *direction = 'd';
+        break;
+      case 'x':
+        *is_game_over = 1;
+      default:
+        break;
     }
-    else
-    {
-        switch (*direction)
-        {
-        case 'w':
-            *y = *y - 1;
-            break;
-        case 's':
-            *y = *y + 1;
-            break;
-        case 'a':
-            *x = *x - 1;
-            break;
-        case 'd':
-            *x = *x + 1;
-            break;
-        default:
-            break;
-        }
-    }
+  }
+
+  switch (*direction) {
+    case 'w':
+      current->y = current->y - 1;
+      break;
+    case 's':
+      current->y = current->y + 1;
+      break;
+    case 'a':
+      current->x = current->x - 1;
+      break;
+    case 'd':
+      current->x = current->x + 1;
+      break;
+    default:
+      break;
+  }
+
+  current = current->next;
+  while (current != NULL) {
+    int curr_x = current->x;
+    int curr_y = current->y;
+    current->x = prev_x;
+    current->y = prev_y;
+    prev_x = curr_x;
+    prev_y = curr_y;
+    current = current->next;
+  }
 }
 
-int main()
-{
-    int score = 0;
-    int is_game_over = 0;
+SnakeNode *add_node(int x, int y) {}
 
-    char direction = 'a';
-    int snake_x = 20;
-    int snake_y = 20;
+int main() {
+  int score = 0;
+  int is_game_over = 0;
 
-    int fruit[2] = {rand() % (1 - BOARD_WIDTH) + 1, rand() % (1 - BOARD_HEIGHT) + 1};
+  char direction = 's';
 
-    while (!is_game_over)
-    {
-        generate_board(snake_x, snake_y, fruit);
-        move(&snake_x, &snake_y, &direction, &is_game_over);
-        if (snake_x == 0 || snake_x == BOARD_WIDTH - 1 || snake_y == 0 || snake_y == BOARD_HEIGHT - 1)
-        {
-            is_game_over = 1;
-        }
-        if (snake_x == fruit[0] && snake_y == fruit[1])
-        {
-            score += 10;
-            fruit[0] = rand() % (1 - BOARD_WIDTH) + 1;
-            fruit[1] = rand() % (1 - BOARD_HEIGHT) + 1;
-        }
-        printf("Score: %d", score);
+  SnakeNode *head = malloc(sizeof(SnakeNode));
+  SnakeNode *tail = malloc(sizeof(SnakeNode));
+  SnakeNode *tail2 = malloc(sizeof(SnakeNode));
+
+  if (head == NULL) {
+    perror("Error while allocating memory");
+  }
+  if (tail == NULL) {
+    perror("Error while allocating memory");
+  }
+  if (tail2 == NULL) {
+    perror("Error while allocating memory");
+  }
+  head->next = tail;
+  head->x = 20;
+  head->y = 20;
+
+  tail->next = tail2;
+  tail->x = 21;
+  tail->y = 20;
+
+  tail2->next = NULL;
+  tail2->x = 22;
+  tail2->y = 20;
+
+  int fruit[2] = {rand() % (1 - BOARD_WIDTH) + 1,
+                  rand() % (1 - BOARD_HEIGHT) + 1};
+
+  while (!is_game_over) {
+    generate_board(head, fruit);
+    move(head, &direction, &is_game_over);
+    if (head->x == 0 || head->x == BOARD_WIDTH - 1 || head->y == 0 ||
+        head->y == BOARD_HEIGHT - 1) {
+      is_game_over = 1;
     }
+    if (head->x == fruit[0] && head->y == fruit[1]) {
+      score += 10;
+      fruit[0] = rand() % (1 - BOARD_WIDTH) + 1;
+      fruit[1] = rand() % (1 - BOARD_HEIGHT) + 1;
+    }
+  }
 
-    return 0;
+  return 0;
 }
